db08e7cf62983e00e8e28412de6ade86acf81a3c
이종준 <ljj9316@naver.com>
2021-11-11 15:06:56 +0900

    1113 푼 것만 업로드

---
+++ 알고리즘스터디/2021_11/1113/이종준/14503_로봇청소기.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+# 북동남서
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+
+def bfs():
+    global ans
+    while q:
+        # 큐에서 빼냄
+        r, c, dir = q.popleft()
+
+        # 네 방향 탐색: 첫 왼쪽
+        nr = r + dr[(dir - 1 + 4) % 4]
+        nc = c + dc[(dir - 1 + 4) % 4]
+        if arr[nr][nc] == 0 and not visited[nr][nc]:
+            visited[nr][nc] = 1
+            ans += 1
+            q.append((nr, nc, (dir - 1 + 4) % 4))
+        else:
+            # 두번째 왼쪽
+            nr = r + dr[(dir - 2 + 4) % 4]
+            nc = c + dc[(dir - 2 + 4) % 4]
+            if arr[nr][nc] == 0 and not visited[nr][nc]:
+                visited[nr][nc] = 1
+                ans += 1
+                q.append((nr, nc, (dir - 2 + 4) % 4))
+            else:
+                # 세번째 왼쪽
+                nr = r + dr[(dir - 3 + 4) % 4]
+                nc = c + dc[(dir - 3 + 4) % 4]
+                if arr[nr][nc] == 0 and not visited[nr][nc]:
+                    visited[nr][nc] = 1
+                    ans += 1
+                    q.append((nr, nc, (dir - 3 + 4) % 4))
+                else:
+                    # 마지막 왼쪽 = 원래 방향
+                    nr = r + dr[(dir - 4 + 4) % 4]
+                    nc = c + dc[(dir - 4 + 4) % 4]
+                    if arr[nr][nc] == 0 and not visited[nr][nc]:
+                        visited[nr][nc] = 1
+                        ans += 1
+                        q.append((nr, nc, dir))
+                    # 후진할 수 있으면 후진
+                    else:
+                        nr = r + dr[(dir - 2 + 4) % 4]
+                        nc = c + dc[(dir - 2 + 4) % 4]
+                        if arr[nr][nc] == 0:
+                            q.append((nr, nc, dir))
+                        elif arr[nr][nc] == 1:
+                            break
+
+
+N, M = map(int, input().split())
+r, c, dir = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 1
+visited = [[0] * M for _ in range(N)]
+q = deque()
+visited[r][c] = 1
+q.append((r, c, dir))
+bfs()
+print(ans)
